0a52a2baf828c95a0faea4d61d5bcbc35105eafb
Sergey <rogozhinis@mail.ru>
2022-12-26 20:25:33 +0500

    domashka2

---
+++ home01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цмфру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+int NumberPlus;
+int Number1;
+int Number2;
+
+
+if (Number >= 0)
+{
+     NumberPlus = Number;
+}
+else
+{
+     NumberPlus = (Number * -1);
+}
+
+
+if (NumberPlus < 100 || NumberPlus > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+     Number1 = NumberPlus/ 10;
+     Number2 = Number1 % 10;
+     Console.WriteLine($"Вывод:{Number2}");
+}
